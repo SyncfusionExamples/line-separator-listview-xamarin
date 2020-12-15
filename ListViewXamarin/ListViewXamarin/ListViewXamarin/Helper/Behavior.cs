4c2419d5efb8d4befd22d8c39f443a8b0b8cf4d4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-15 11:20:16 +0530

    XAMARIN-38102 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Field
+
+        SfListView ListView;
+        #endregion
+
+        #region Overrides
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.DragDropController.UpdateSource = true;
+            ListView.ItemDragging += ListView_ItemDragging;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView.ItemDragging -= ListView_ItemDragging;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Events
+
+        private void ListView_ItemDragging(object sender, ItemDraggingEventArgs e)
+        {
+            if (e.Action == DragAction.Drop)
+            {
+                Device.BeginInvokeOnMainThread(() => ListView.RefreshListViewItem(e.OldIndex, e.NewIndex, true));
+            }
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/SeparatorVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class SeparatorVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var listView = parameter as SfListView;
+
+            if (value == null)
+                return false;
+
+            return listView.DataSource.DisplayItems[listView.DataSource.DisplayItems.Count - 1] != value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
